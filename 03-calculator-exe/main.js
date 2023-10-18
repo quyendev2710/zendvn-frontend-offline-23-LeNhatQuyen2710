fddf8ee42403d182d5293850014849e57080acb8
Quyen Nhat Le <lenhatquyen8762@gmail.com>
2023-10-18 22:45:13 +0700

    Calculator-Exe-deadline

---
+++ 03-calculator-exe/main.js
@@ -0,0 +1,46 @@
+const elNumberOne = document.getElementById('number-one');
+const elNumberTwo = document.getElementById('number-two');
+const elBtnSum = document.getElementById('btn-sum');
+const elBtnSubtract = document.getElementById('btn-subtract');
+const elBtnMultiply = document.getElementById('btn-multiply');
+const elBtnDivide = document.getElementById('btn-divide');
+const elResult = document.getElementById('result');
+
+function calculator(operator = '') {
+    const numberOne = parseInt(elNumberOne.value);
+    const numberTwo = parseInt(elNumberTwo.value);
+
+    if (!numberOne || !numberTwo) {
+        alert('Vui lòng nhập đủ 2 số');
+    }
+
+    let result;
+    switch (operator) {
+        case '+':
+            result = numberOne + numberTwo;
+            break;
+        case '-':
+            result = numberOne - numberTwo;
+            break;
+        case '*':
+            result = numberOne * numberTwo;
+            break;
+        case '/':
+            result = numberOne / numberTwo;
+            break;
+    }
+    elResult.innerText = result;
+}
+
+elBtnSum.addEventListener('click', function () {
+    calculator('+');
+});
+elBtnSubtract.addEventListener('click', function () {
+    calculator('-');
+});
+elBtnMultiply.addEventListener('click', function () {
+    calculator('*');
+});
+elBtnDivide.addEventListener('click', function () {
+    calculator('/');
+});
