ec48ccc51df2fc025c4b7e1605bb027bd5e15a76
Quyen Nhat Le <lenhatquyen8762@gmail.com>
2023-11-04 16:27:59 +0700

    todo-app-adv-2

--- todo-app-adv/main.js
+++ todo-app-adv/main.js
@@ -69,35 +69,166 @@ const elLevel = document.getElementById('level');
 const elbtnSave = document.getElementById('btnSave');
 const elList = document.getElementById('list');
 const elTodoForm = document.getElementById('todo-form');
+const elStatusFilter = document.getElementById('statusFilter');
+const elLevelFilter = document.getElementById('levelFilter');
+const elIncreaseName = document.getElementById('increaseName');
+const elDecreaseName = document.getElementById('decreaseName');
+const elIncreaseLevel = document.getElementById('increaseLevel');
+const elDecreaseLevel = document.getElementById('decreaseLevel');
 
 
+// Hien thi danh sach
 renderTodoList(todos);
 
-elTodoForm.addEventListener('submit', function(e){
+elTodoForm.addEventListener('submit', function (e) {
     e.preventDefault();
     let valueName = elInputSave.value;
     console.log(valueName);
     let valueLevel = elLevel.value;
     let newTodo = {
         id: self.crypto.randomUUID(),
-        name : valueName,
-        status : false,
+        name: valueName,
+        status: false,
         level: valueLevel,
     }
     todos.push(newTodo);
     renderTodoList(todos);
     elInputSave.value = '';
-})
+});
 
-elBtnSearch.addEventListener('click', function(){
+elBtnSearch.addEventListener('click', function () {
     let valueSearch = elInputSearch.value;
     console.log(valueSearch);
     let searchList = todos.filter((item) => {
-       return item.name.toLowerCase().includes(valueSearch.toLowerCase());
+        return item.name.toLowerCase().includes(valueSearch.toLowerCase());
     });
     renderTodoList(searchList);
+});
+
+
+elList.addEventListener('click', function (e) {
+    const el = e.target;
+    if (el.classList.contains('btn-delete')) {
+        const idBtnDelete = el.id;
+        todos = todos.filter((item) => item.id !== idBtnDelete);
+        renderTodoList(todos);
+    }
+    if (el.classList.contains('todo-name')) {
+        const idTodoStatus = el.id;
+        const idx = todos.findIndex(item => item.id == idTodoStatus);
+        console.log(idx);
+        todos[idx].status = !todos[idx].status;
+        renderTodoList(todos);
+    }
+
+});
+
+
+elStatusFilter.addEventListener('change', function () {
+    let valueStatusFilter = elStatusFilter.value;
+    let newTodo = [];
+    switch (valueStatusFilter) {
+        case "all":
+            renderTodoList(todos);
+            break;
+        case "1":
+            newTodo = todos.filter(item => item.status == true);
+            renderTodoList(newTodo);
+            break;
+        case "0":
+            newTodo = todos.filter(item => item.status == false)
+            renderTodoList(newTodo);
+            break;
+    }
 })
 
+elLevelFilter.addEventListener('change', function () {
+    let valueLevelFilter = elLevelFilter.value;
+    let newTodo = [];
+    switch (valueLevelFilter) {
+        case "all":
+            renderTodoList(todos);
+            break;
+        case '1':
+            newTodo = todos.filter(item => item.level == 1)
+            renderTodoList(newTodo);
+            break;
+        case '2':
+            newTodo = todos.filter(item => item.level == 2)
+            renderTodoList(newTodo);
+            break;
+        case '3':
+            newTodo = todos.filter(item => item.level == 3)
+            renderTodoList(newTodo);
+            break;
+    }
+})
+
+elIncreaseName.addEventListener('click', function () {
+    sortList("name", "asc");
+})
+
+elDecreaseName.addEventListener('click', function () {
+    sortList("name", "desc");
+})
+
+elIncreaseLevel.addEventListener('click', function () {
+    sortList("level", "asc");
+})
+
+elDecreaseLevel.addEventListener('click', function () {
+    sortList("level", "desc");
+})
+
+
+function sortList(sortType, sortDir) {
+    let coppyTodos = [...todos];
+    switch (sortType) {
+        case "name":
+            if (sortDir === "asc") {
+                coppyTodos.sort((a, b) => {
+                    a = a.name.toLowerCase();
+                    b = b.name.toLowerCase();
+                    if (a < b) return -1;
+                    if (a > b) return 1;
+                    return 0;
+                })
+                renderTodoList(coppyTodos);
+            } else {
+                coppyTodos.sort((a, b) => {
+                    a = a.name.toLowerCase();
+                    b = b.name.toLowerCase();
+                    if (a < b) return 1;
+                    if (a > b) return -1;
+                    return 0;
+                })
+                renderTodoList(coppyTodos);
+            }
+            console.log(coppyTodos);
+            break;
+        case "level":
+            if (sortDir === "asc") {
+                coppyTodos.sort((a, b) => {
+                    a = a.level;
+                    b = b.level;
+                    if (a < b) return -1;
+                    if (a > b) return 1;
+                    return 0;
+                })
+                renderTodoList(coppyTodos);
+            } else {
+                coppyTodos.sort((a, b) => {
+                    a = a.level;
+                    b = b.level;
+                    if (a < b) return 1;
+                    if (a > b) return -1;
+                    return 0;
+                })
+                renderTodoList(coppyTodos);
+            }
+            break;
+    }
+}
 
 function renderTodoList(list) {
     let htmlTodos = "";
@@ -112,17 +243,16 @@ function renderTodoList(list) {
         } else {
             levelName = 'Khó';
         }
+        const status = items.status ? 'completed' : '';
         htmlTodos += /* html */`
         <tr>
-            <td>${items.name}</td>
-            <td>${completed}</td>
+            <td class="todo-name ${status}" id="${items.id}">${items.name}</td>
             <td>${levelName}</td>
             <td>
                 <button>Sửa</button>
-                <button>Xoá</button>
+                <button class="btn-delete" id="${items.id}">Xoá</button>
             </td> 
         </tr>`
     })
     elList.innerHTML = htmlTodos;
-}
-
+};
